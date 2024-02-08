42b31fed53d12c2402fc4184d9e20f71efd26af4
YuliiaaLevchenko <141173167+YuliiaaLevchenko@users.noreply.github.com>
2024-02-08 19:29:38 +0200

    change 1

---
+++ src/components/Alert.jsx
@@ -0,0 +1,14 @@
+//src/components/Alert.jsx
+
+const alertStyles = {
+    margin: 8,
+    padding: "12px 16px",
+    borderRadius: 4,
+    backgroundColor: "gray",
+    color: "white",
+  };
+  
+  export const Alert = ({ children }) => {
+    return <p style={alertStyles}>{children}</p>;
+  };
+  
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 // src/components/App.jsx
 
+import { Product } from "./Product";
+
 export default function App() {
   return (
     <div>
@@ -7,11 +9,12 @@ export default function App() {
 
       <Product
         name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
         price={10.99}
       />
       <Product
         name="Fries and Burger"
-        imgUrl="<https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640>"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
         price={14.29}
       />
     </div>
